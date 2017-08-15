75b6c139ce45e852c69a18ae355721b03d396be2
danielVebman <danveb800@gmail.com>
2017-08-16 01:55:40 +0300

    Update StatusBarViewController.swift

--- StatusBarViewController.swift
+++ StatusBarViewController.swift
@@ -10,26 +10,42 @@ import Foundation
 import UIKit
 
 class StatusBarViewController: UIViewController, FadeAnimatorDelegate {
+    private var blurView: UIVisualEffectView?
+    
     private var timeLabel: TimeLabel!
     private var networkLabel: NetworkLabel!
     private var batteryView: BatteryView!
     
     private var animator: FadeAnimator!
     
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        timeLabel = TimeLabel()
+        networkLabel = NetworkLabel()
+        batteryView = BatteryView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         view.frame.size.height = MinimalistStatusBar.shared.height
         
-        timeLabel = TimeLabel(frame: view.bounds)
+        blurView = UIVisualEffectView(frame: view.bounds)
+        view.addSubview(blurView!)
+        
+        timeLabel.frame = view.bounds
         timeLabel.frame.origin.x -= 5
         timeLabel.alpha = 1
         view.addSubview(timeLabel)
         
-        networkLabel = NetworkLabel(frame: view.bounds)
+        networkLabel.frame = view.bounds
         networkLabel.frame.origin.x -= 5
         networkLabel.alpha = 0
         view.addSubview(networkLabel)
         
-        batteryView = BatteryView(frame: view.bounds)
+        batteryView.frame = view.bounds
         batteryView.frame.origin.x -= 5
         batteryView.alpha = 0
         view.addSubview(batteryView)
@@ -57,5 +73,9 @@ class StatusBarViewController: UIViewController, FadeAnimatorDelegate {
             networkLabel.textColor = color
             batteryView.tintColor = color
         }
+    }
+    
+    func setBlurViewVisible(_ visible: Bool) {
+        blurView?.effect = visible ? UIBlurEffect(style: .regular) : nil
     }
 }
